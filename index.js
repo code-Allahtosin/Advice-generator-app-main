460906add897c2eeb9b3671fbe0860bb6724bfbb
code-Allahtosin <codeallahtosin+git@gmail.com>
2022-09-04 17:39:34 +0100

    desktop view preliminaries complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const id = document.getElementById('advice-number')
+const advice = document.getElementById("advice-text")
+const anotherAdvice= document.getElementById('another-advice')
 
+// const fetch = require("node-fetch").fetch
+// import fetch from "node-fetch"
+
+fetchAdvice()
+anotherAdvice.addEventListener("click", fetchAdvice)
+async function fetchAdvice() {
+    try {   
+           let response= await fetch("https://api.adviceslip.com/advice")
+            let data = await response.json()
+            id.textContent = await data.slip.id
+            advice.textContent= await data.slip.advice
+            return
+        }  catch (error) {
+             console.log(error)
+            }
+        }        
